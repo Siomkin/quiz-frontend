bf50ce3c7f0350a9a8e939a8595a569f9445a682
Siomkin Alexander <siomkin.alexander@gmail.com>
2019-05-03 12:57:52 +0300

    Use spin with fetching parameter

--- src/components/Common/Spinner.js
+++ src/components/Common/Spinner.js
@@ -3,17 +3,23 @@ import React from 'react';
 export class Spinner extends React.Component {
     render() {
 
+        const {spin} = this.props;
         let text = this.props.children;
 
-        return (
-            <div className="text-center">
-                <i className="fas fa-spinner fa-spin"/>
-                {
-                    text && (
-                        <span>{text}</span>
-                    )
-                }
-            </div>
-        );
+        if (spin !== false) {
+            return (
+                <div className="text-center">
+                    <i className="fas fa-spinner fa-spin"/>
+                    {
+                        text && (
+                            <span>{text}</span>
+                        )
+                    }
+                </div>
+            );
+        }
+
+        return ('');
+
     }
 }
